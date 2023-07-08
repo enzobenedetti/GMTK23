abeb492fb39e3762b216003a7de1e01e4ecc9010
enzobenedetti <enzob210401@gmail.com>
2023-07-08 09:55:39 +0200

    Game manager script DontDestroyOnLoad added

---
+++ Assets/Script/GameManager.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using UnityEngine;
+
+namespace Script
+{
+    public class GameManager : MonoBehaviour
+    {
+        private void Awake()
+        {
+            if (FindObjectsByType<GameManager>(FindObjectsSortMode.None).Length > 1)
+            {
+                Destroy(transform);
+            }
+            DontDestroyOnLoad(transform);
+        }
+
+        public void WinGame()
+        {
+            //TODO show win UI
+        }
+
+        public void LostGame()
+        {
+            //TODO show lost UI
+        }
+    }
+}
